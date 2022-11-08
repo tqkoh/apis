8907b7b1006cc0adc59557072ee51e02fc53507f
tqkoh <shktmygs@gmail.com>
2022-11-08 16:18:49 +0900

    add tokuteibu

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,20 +1,24 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "https://petstore.swagger.io/v2/swagger.json",
-		url: "specs/index.yaml",
-    dom_id: '#swagger-ui',
+    urls: [
+      {
+        url: "specs/qkjudge.yaml",
+        name: "QK Judge",
+      },
+      {
+        url: "specs/tokuteibu.yaml",
+        name: "tokuteibu",
+      },
+    ],
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
